ccecbf4fc1d3119f15c50795f6897a101c198bda
divdot <157302347+mrjabka@users.noreply.github.com>
2026-06-08 19:31:05 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://www.cubeyond.net/",
+  site: "https://www.divdot.ru/",
   vite: {
     plugins: [tailwindcss()]
   }
